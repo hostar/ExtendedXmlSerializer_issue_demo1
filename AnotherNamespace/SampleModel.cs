3e7e2d41b23daa89d742c1c7fbe93a3262d9dc57
hostar <janstarka@gmail.com>
2020-09-13 19:06:40 +0200

    Add project files.

---
+++ AnotherNamespace/SampleModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnotherNamespace
+{
+    public class SampleModel
+    {
+        private string firstName = "default_first_name";
+        public string FirstName 
+        {
+            get => firstName; 
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    firstName = value;
+                }
+            }
+        }
+
+        private string lastName = "default_last_name";
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    lastName = value;
+                }
+            }
+        }
+
+        private List<SampleItem> listOfItems = null;
+        public List<SampleItem> ListOfItems
+        {
+            get
+            {
+                if (listOfItems == null)
+                {
+                    listOfItems = new List<SampleItem> { 
+                        new SampleItem 
+                        { 
+                            ItemName = "item1",
+                            ItemValue = 1
+                        },
+                        new SampleItem
+                        {
+                            ItemName = "item2",
+                            ItemValue = 2
+                        }
+                    };
+                }
+                return listOfItems;
+            }
+
+            set => listOfItems = value;
+        }
+    }
+
+    public class SampleItem
+    {
+        public string ItemName { get; set; }
+        public int ItemValue { get; set; }
+    }
+}
---
+++ XmlRead1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using AnotherNamespace;
+using ExtendedXmlSerializer;
+using ExtendedXmlSerializer.Configuration;
+
+namespace RunMe
+{
+    class Program
+    {
+        static void Main()
+        {
+            var serializer = new ConfigurationContainer()
+                                                         .EnableImplicitTyping(typeof(SampleModel))
+                                                         //.EnableReferences()
+                                                         .Create();
+
+            Console.WriteLine($"input1");
+            DeserializeTest(serializer, "input1.xml");
+            Console.WriteLine($"---------------------------------");
+
+            Console.WriteLine($"input2");
+            DeserializeTest(serializer, "input2.xml");
+
+            Console.WriteLine("LastName should be empty instead of new line.");
+            Console.WriteLine($"Press enter to exit.");
+            Console.ReadLine();
+        }
+
+        private static void DeserializeTest(IExtendedXmlSerializer serializer, string fname)
+        {
+            string fileContent = File.ReadAllText(Directory.GetCurrentDirectory() + @$"\Data\{fname}");
+
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(fileContent);
+            writer.Flush();
+            stream.Position = 0;
+
+            using XmlTextReader reader = new XmlTextReader(stream) { Normalization = false };
+            var result = (SampleModel)serializer.Deserialize(reader);
+
+            Console.WriteLine($"{nameof(result.ListOfItems)} count: {result.ListOfItems.Count}");
+            Console.WriteLine($"{nameof(result.LastName)}: {result.LastName}");
+        }
+    }
+}
